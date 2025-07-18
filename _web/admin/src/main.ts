9050e8e3f236c8746398ac37674a9277ced0153d
hezhilong <1092501244@qq.com>
2025-07-18 16:22:56 +0800

    移除百度map

--- _web/admin/src/main.ts
+++ _web/admin/src/main.ts
@@ -10,13 +10,10 @@ import { store } from './store';
 import 'tdesign-vue-next/es/style/index.css';
 import '@/style/index.less';
 import './permission';
-import BaiduMap from 'vue-baidu-map-3x';
 
 import '@/assets/theme/tencent-cloud.css';
 
 const app = createApp(App);
 
-app.use(TDesign).use(store).use(router).use(TDesignChat).use(BaiduMap, {
-  ak: import.meta.env.VITE_APP_BAIDU_MAP_AK,
-});
+app.use(TDesign).use(store).use(router).use(TDesignChat);
 app.mount('#app');
