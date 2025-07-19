0fb1df3f0bec81c691ab34c02ca94edf052fbb92
hezhilong <1092501244@qq.com>
2025-07-19 15:17:13 +0800

    gen demo

--- ic-framework-project/src/test/java/cn/icframework/test/TestGen.java
+++ ic-framework-project/src/test/java/cn/icframework/test/TestGen.java
@@ -1,9 +1,28 @@
 package cn.icframework.test;
 
 
+import cn.icframework.gen.Generator;
+import org.junit.jupiter.api.Test;
+
 /**
  * @author iceFire
  * @since 2023/6/5
  */
 public class TestGen {
+    @Test
+    public void genCode() {
+        Generator generator = new Generator();
+        // 作者
+        generator.author("iceFire");
+        // 指定模块名称
+        generator.moduleName("dep");
+        // 指定需要生成的实体类
+//        generator.tableClass(cn.icframework.project.module.dep.Dept.class);
+        // 指定生成的java文件路径
+        generator.javaPath("D:\\product\\ic\\ic-framework-service\\ic-framework-project\\src\\main\\java");
+        // 指定生成的vue文件路径
+        generator.pageVueSrcPath("D:\\product\\ic\\ic-framework-service\\_web\\admin\\src");
+        // 生成
+        generator.build();
+    }
 }
